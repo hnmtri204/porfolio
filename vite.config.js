16921d4fe3000c51a54b17fcb5ef68a9d234ef25
hnmtri204 <hoangngocminhtri.thpt.ht@gmail.com>
2025-01-06 17:02:43 +0700

    Fix GitHub Pages path

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: '/hnmtri204.github.io/'
 });
